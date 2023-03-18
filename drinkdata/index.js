e0d8ca247f9fbfd05ca2e3487fcdfa9da06dac03
haywo <ru8391@gmail.com>
2023-03-18 14:52:36 +0800

    mongodb init

--- drinkdata/index.js
+++ drinkdata/index.js
@@ -1,10 +1,32 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 
 const app = express();
 const port = 3014;
+dotenv.config();
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log('connect to mongodb succeed'); //test database connection
+    } catch (error) {
+        console.log('disconnect to mongodb'); //test database connection
+    }
+}
+
+mongoose.connection.on('connected', () => {
+    console.log('connected mongodb');
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('disconnected mongodb');
+});
+
+
 
 app.listen(port, () => {
+    connect();
     console.log(`connect to ${port} succeed`);
 })
